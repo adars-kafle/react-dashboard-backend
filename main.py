b47a8448af161fc18e8a7e039bfa76fb53b668d7
adars124 <lonleyau@gmail.com>
2024-08-27 16:07:56 +0545

    initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI() # Initialize the object
+
+@app.get("/")
+def root() -> None:
+    return {"message": "Hello world!"}
+
+@app.get("/getSuppliers")
+def get_suppliers():

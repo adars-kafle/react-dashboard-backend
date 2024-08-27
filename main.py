8bb0c603d035958a15c31f4804cfa910016fe113
adars124 <lonleyau@gmail.com>
2024-08-27 16:18:28 +0545

    added routes for handling suppliers in main

--- main.py
+++ main.py
@@ -1,13 +1,38 @@
-from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
-from models import Supplier
+from fastapi import Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+from . import crud, models, schemas
+from .database import engine, get_db
 
-app = FastAPI() # Initialize the object
+models.Base.metadata.create_all(bind=engine)
 
-@app.get("/")
-def root() -> None:
-    return {"message": "Hello world!"}
+app = FastAPI()
 
-@app.get("/getSuppliers")
-def get_suppliers() -> list[Supplier]:
-    pass
+@app.post("/suppliers/", response_model=schemas.Supplier)
+def create_supplier(supplier: schemas.SupplierCreate, db: Session = Depends(get_db)):
+    return crud.create_supplier(db=db, supplier=supplier)
+
+@app.get("/suppliers/", response_model=list[schemas.Supplier])
+def read_suppliers(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    suppliers = crud.get_suppliers(db, skip=skip, limit=limit)
+    return suppliers
+
+@app.get("/suppliers/{supplier_id}", response_model=schemas.Supplier)
+def read_supplier(supplier_id: int, db: Session = Depends(get_db)):
+    db_supplier = crud.get_supplier(db, supplier_id=supplier_id)
+    if db_supplier is None:
+        raise HTTPException(status_code=404, detail="Supplier not found")
+    return db_supplier
+
+@app.put("/suppliers/{supplier_id}", response_model=schemas.Supplier)
+def update_supplier(supplier_id: int, supplier: schemas.SupplierCreate, db: Session = Depends(get_db)):
+    db_supplier = crud.update_supplier(db, supplier_id=supplier_id, supplier=supplier)
+    if db_supplier is None:
+        raise HTTPException(status_code=404, detail="Supplier not found")
+    return db_supplier
+
+@app.delete("/suppliers/{supplier_id}", response_model=schemas.Supplier)
+def delete_supplier(supplier_id: int, db: Session = Depends(get_db)):
+    db_supplier = crud.delete_supplier(db, supplier_id=supplier_id)
+    if db_supplier is None:
+        raise HTTPException(status_code=404, detail="Supplier not found")
+    return db_supplier
